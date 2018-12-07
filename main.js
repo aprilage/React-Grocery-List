9e50d2893c904d2eef0f99a27708760aa969fa16
April Vanderslice <aprilage@icloud.com>
2018-12-07 07:19:47 -0600

    made a checklist of next steps

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+/* @TODO
+- change to use JSX inside of grocery list Component
+- add items to GroceryList
+  - create a GroceryInput component
+    - text input
+    - add button
+    - accept a submit handler through props to get value from input
+  - create local state in GroceryList.state.list
+    - through the submit handler append to state.list
+    - render GroceryItem based on state.list 
+*/
+
 class GroceryList extends React.Component {
   constructor(props) {
     super(props)
